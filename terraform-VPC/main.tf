81c9cb87ac25567a1c52c3ad6181df624447efe4
Hubert Piotrowski <sugar.shad0w111@gmail.com>
2022-11-09 07:41:41 +0100

    fmt

--- terraform-VPC/main.tf
+++ terraform-VPC/main.tf
@@ -1,79 +1,79 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-     Name = "main"
-    }
+  tags = {
+    Name = "main"
+  }
 
 }
 
 resource "aws_subnet" "publicsub0" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
-      tags = {
-     Name = "publicsub0"
-    }
+  tags = {
+    Name = "publicsub0"
+  }
 }
 
 resource "aws_subnet" "publicsub1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-     tags = {
-     Name = "publicsub1"
-    }
+  tags = {
+    Name = "publicsub1"
+  }
 }
 
 resource "aws_subnet" "privatesub0" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
-     tags = {
-     Name = "privatesub0"
-    }
+  tags = {
+    Name = "privatesub0"
+  }
 }
 
 resource "aws_subnet" "privatesub1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
-      tags = {
-     Name = "privatesub1"
-    }
+  tags = {
+    Name = "privatesub1"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
-        tags = {
-     Name = "main"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main"
+  }
 }
 
 resource "aws_eip" "nat0" {
-  vpc      = true
-      tags = {
-     Name = "nat0"
-    }
+  vpc = true
+  tags = {
+    Name = "nat0"
+  }
 }
 
 resource "aws_eip" "nat1" {
-  vpc      = true
-      tags = {
-     Name = "nat1"
-    }
+  vpc = true
+  tags = {
+    Name = "nat1"
+  }
 }
 
 resource "aws_nat_gateway" "maingw0" {
   allocation_id = aws_eip.nat0.id
   subnet_id     = aws_subnet.publicsub0.id
-        tags = {
-     Name = "maingw0"
-    }
+  tags = {
+    Name = "maingw0"
+  }
 }
 
 resource "aws_nat_gateway" "maingw1" {
   allocation_id = aws_eip.nat1.id
   subnet_id     = aws_subnet.publicsub1.id
-        tags = {
-     Name = "maingw1"
-    }
+  tags = {
+    Name = "maingw1"
+  }
 }
 
 resource "aws_route_table" "public" {
@@ -83,9 +83,9 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
-          tags = {
-     Name = "public"
-    }
+  tags = {
+    Name = "public"
+  }
 }
 
 resource "aws_route_table" "private0" {
@@ -95,9 +95,9 @@ resource "aws_route_table" "private0" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.maingw0.id
   }
-          tags = {
-     Name = "private0"
-    }
+  tags = {
+    Name = "private0"
+  }
 }
 
 resource "aws_route_table" "private1" {
@@ -107,9 +107,9 @@ resource "aws_route_table" "private1" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.maingw1.id
   }
-          tags = {
-     Name = "private1"
-    }
+  tags = {
+    Name = "private1"
+  }
 }
 
 resource "aws_route_table_association" "public0" {
