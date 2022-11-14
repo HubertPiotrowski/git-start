1113f8f0cd0d5084c3799812eff13444c10dee3c
Hubert Piotrowski <sugar.shad0w111@gmail.com>
2022-11-14 10:24:47 +0100

    formatting

--- terraform-EC2/instance.tf
+++ terraform-EC2/instance.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "public" {
-  ami = "ami-0648ea225c13e0729"
+  ami                         = "ami-0648ea225c13e0729"
   associate_public_ip_address = true
-  instance_type = "t3.micro"
-  key_name = "main"
-  vpc_security_group_ids = [aws_security_group.public.id]
-  subnet_id      = aws_subnet.publicsub[0].id
+  instance_type               = "t3.micro"
+  key_name                    = "main"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = aws_subnet.publicsub[0].id
   tags = {
     Name = "${var.env_code}-public"
   }
@@ -16,18 +16,18 @@ resource "aws_security_group" "public" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "SSH from public"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["83.5.151.173/32"]
+    description = "SSH from public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["83.5.151.173/32"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -37,11 +37,11 @@ resource "aws_security_group" "public" {
 
 
 resource "aws_instance" "private" {
-  ami = "ami-0648ea225c13e0729"
-  instance_type = "t3.micro"
-  key_name = "main"
+  ami                    = "ami-0648ea225c13e0729"
+  instance_type          = "t3.micro"
+  key_name               = "main"
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id      = aws_subnet.privatesub[0].id
+  subnet_id              = aws_subnet.privatesub[0].id
   tags = {
     Name = "${var.env_code}-private"
   }
@@ -53,18 +53,18 @@ resource "aws_security_group" "private" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [var.vpc_cidr]
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
