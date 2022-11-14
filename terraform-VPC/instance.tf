c1b0c3fdf399944ab858bd2739a86a235f2129dc
Hubert Piotrowski <sugar.shad0w111@gmail.com>
2022-11-14 12:47:41 +0100

    adding keys

--- terraform-VPC/instance.tf
+++ terraform-VPC/instance.tf
@@ -1,8 +1,43 @@
+data "aws_ami" "amazonlinux" {
+  most_recent      = true
+
+filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners           = [137112412989]
+
+}
+
+#key pair variable
+resource "aws_key_pair" "my_key_pair" {
+    key_name   = "${uuid()}"
+    public_key = "${tls_private_key.t.public_key_openssh}"
+}
+provider "tls" {}
+resource "tls_private_key" "t" {
+    algorithm = "RSA"
+}
+provider "local" {}
+resource "local_file" "key" {
+    content  = "${tls_private_key.t.private_key_pem}"
+    filename = "id_rsa"
+    provisioner "local-exec" {
+        command = "chmod 600 id_rsa"
+    }
+}
+
+
 resource "aws_instance" "public" {
-  ami                         = "ami-0648ea225c13e0729"
+  ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
   instance_type               = "t3.micro"
-  key_name                    = "main"
+  key_name                    = aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = aws_subnet.publicsub[0].id
   tags = {
@@ -37,9 +72,9 @@ resource "aws_security_group" "public" {
 
 
 resource "aws_instance" "private" {
-  ami                    = "ami-0648ea225c13e0729"
+  ami                    = data.aws_ami.amazonlinux.id
   instance_type          = "t3.micro"
-  key_name               = "main"
+  key_name               = aws_key_pair.my_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.private.id]
   subnet_id              = aws_subnet.privatesub[0].id
   tags = {
