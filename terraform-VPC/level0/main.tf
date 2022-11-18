35a46926af2887c99697a3624e293bb814a38881
Hubert Piotrowski <sugar.shad0w111@gmail.com>
2022-11-18 12:55:58 +0100

    bucket creation

---
+++ terraform-VPC/level0/main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "tf-state-hubert132487863"
+}
+
+resource "aws_dynamodb_table" "tfstate" {
+  name           = "tf-state"
+  billing_mode   = "PROVISIONED"
+    read_capacity  = 1
+    write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ terraform-VPC/level0/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~>3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+    allowed_account_ids = [
+        "405263612440",
+    ]
+}
