f66704ca1ea652efd7a5d64140fbefd66e2b5dbe
Hubert Piotrowski <sugar.shad0w111@gmail.com>
2022-12-05 11:56:04 +0100

    fmt

--- terraform-VPC/level2/asg.tf
+++ terraform-VPC/level2/asg.tf
@@ -1,9 +1,9 @@
-module asg {
-    source = "../modules/asg"
+module "asg" {
+  source = "../modules/asg"
 
-    env_code = var.env_code
-    vpc_id = data.terraform_remote_state.level1.outputs.vpc_id
-    privatesub_id = data.terraform_remote_state.level1.outputs.privatesub_id
-    load_balancer_sg = aws_security_group.load_balancer.id
-    target_group_arn = aws_alb_target_group.main.id
+  env_code         = var.env_code
+  vpc_id           = data.terraform_remote_state.level1.outputs.vpc_id
+  privatesub_id    = data.terraform_remote_state.level1.outputs.privatesub_id
+  load_balancer_sg = module.lb.load_balancer_sg
+  target_group_arn = module.lb.target_group_arn
 }
--- terraform-VPC/level2/load_balancer.tf
+++ terraform-VPC/level2/load_balancer.tf
@@ -1,72 +1,8 @@
-resource "aws_security_group" "load_balancer" {
-  name        = "${var.env_code}-load_balancer"
-  description = "Allow port 80 inbound to ELB"
-  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+module "lb" {
+  source = "../modules/lb"
 
-  ingress {
-    description = "HTTP to ELB"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  env_code     = var.env_code
+  vpc_id       = data.terraform_remote_state.level1.outputs.vpc_id
+  publicsub_id = data.terraform_remote_state.level1.outputs.publicsub_id
 
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.env_code}-load_balancer"
-  }
-}
-
-resource "aws_alb" "main" {
-  name               = var.env_code
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.load_balancer.id]
-  subnets            = data.terraform_remote_state.level1.outputs.publicsub_id
-
-  tags = {
-    Name = var.env_code
-  }
-
-}
-
-resource "aws_alb_target_group" "main" {
-  name     = var.env_code
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.terraform_remote_state.level1.outputs.vpc_id
-
-  health_check {
-    enabled             = true
-    path                = "/"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 30
-    matcher             = 200
-  }
-
-  tags = {
-    Name = var.env_code
-  }
-
-}
-
-resource "aws_alb_listener" "main" {
-  load_balancer_arn = aws_alb.main.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_alb_target_group.main.arn
-  }
 }
